8d60cab5c8ac6c0474cf8c575cf0f994e93c9ba1
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-05-10 22:20:41 +0100

    Completed UI of new team members section of the manage team page

--- app/(pages)/(main)/settings/manage-team/page.tsx
+++ app/(pages)/(main)/settings/manage-team/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+import ActiveTeamMembersSection from "./sections/ActiveTeamMembersSection";
+import NewTeamMemberSection from "./sections/NewTeamMemberSection";
+
 const ManageTeam = () => {
+    
     return (
         <section className="grow">
+            <NewTeamMemberSection />
+            <ActiveTeamMembersSection />
         </section>
     );
 }
