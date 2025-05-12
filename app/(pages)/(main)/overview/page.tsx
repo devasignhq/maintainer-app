932b9a8ea59cd33bbaef95d65129b499c9058329
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-05-12 22:24:51 +0100

    Completed base layer of wallet page UI

---
+++ app/(pages)/(main)/overview/page.tsx
@@ -0,0 +1,8 @@
+const Overview = () => {
+    return (
+        <div className="w-[80%] mx-auto">
+        </div>
+    )
+}
+ 
+export default Overview;
--- app/(pages)/(main)/wallet/page.tsx
+++ app/(pages)/(main)/wallet/page.tsx
@@ -1,13 +1,99 @@
+"use client";
+import ButtonPrimary from "@/app/components/ButtonPrimary";
+import { useState } from "react";
+import { FiArrowDownRight } from "react-icons/fi";
+import { HiPlus } from "react-icons/hi";
+import { LiaExchangeAltSolid } from "react-icons/lia";
+
 const Wallet = () => {
+    const [activeTab, setActiveTab] = useState(tabs[0]);
+
     return (
         <div className="w-[80%] mx-auto">
-            <div className="w-full grid grid-cols-4 gap-5 my-[30px]">
-                <div className="p-5 border border-primary-200 space-y-2.5">
-                    <h6 className="text-body-small text-light-100"></h6>
+            <section className="w-full space-y-5 mt-[30px] mb-[50px]">
+                <div className="flex items-start justify-between">
+                    <h1 className="text-display-small text-light-100">Project Wallet</h1>
+                    <div className="flex gap-2.5">
+                        <ButtonPrimary
+                            format="SOLID"
+                            text="Withdraw"
+                            sideItem={<FiArrowDownRight />}
+                            attributes={{
+                                onClick: () => {},
+                            }}
+                            extendedClassName="bg-primary-400"
+                        />
+                        <ButtonPrimary
+                            format="SOLID"
+                            text="Top Up"
+                            sideItem={<HiPlus />}
+                            attributes={{
+                                onClick: () => {},
+                            }}
+                        />
+                    </div>
                 </div>
-            </div>
+                <div className="w-full grid grid-cols-2 gap-5">
+                    <div className="w-full p-5 border border-primary-200 flex items-start justify-between">
+                        <div className="space-y-2.5">
+                            <p className="text-body-small text-dark-100">XLM Balance</p>
+                            <p className="text-primary-400">
+                                <span className="text-display-large">5,538</span>
+                                <span className="text-body-medium">.99 XLM</span>
+                            </p>
+                        </div>
+                        <button 
+                            className="flex items-center gap-[5px] text-primary-100 text-button-large font-extrabold hover:text-light-100"
+                            onClick={() => {}}
+                        >
+                            <span>Swap to USDC</span>
+                            <LiaExchangeAltSolid className="text-[22px]" />
+                        </button>
+                    </div>
+                    <div className="w-full p-5 border border-dark-200 flex items-start justify-between">
+                        <div className="space-y-2.5">
+                            <p className="text-body-small text-dark-100">USDC Balance</p>
+                            <p className="text-light-100">
+                                <span className="text-display-large">$1,598</span>
+                                <span className="text-headline-large">.00</span>
+                            </p>
+                        </div>
+                        <button 
+                            className="flex items-center gap-[5px] text-primary-100 text-button-large font-extrabold hover:text-light-100"
+                            onClick={() => {}}
+                        >
+                            <span>Swap to XLM</span>
+                            <LiaExchangeAltSolid className="text-[22px]" />
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="flex gap-2.5 mb-[30px]">
+                    {tabs.map((tab) => (
+                        <button
+                            key={tab.enum}
+                            className={`p-2 px-2.5 text-body-small border 
+                                ${activeTab.enum === tab.enum 
+                                    ? "border-transparent bg-light-100 text-dark-500 font-bold" 
+                                    : "border-dark-100 text-dark-100 hover:text-light-100"}`
+                            }
+                            onClick={() => setActiveTab(tab)}
+                        >
+                            {tab.title}
+                        </button>
+                    ))}
+                </div>
+            </section>
         </div>
     )
 }
  
 export default Wallet;
+
+const tabs = [
+    { title: "All", enum: "ALL" },
+    { title: "Bounty", enum: "BOUNTY" },
+    { title: "Top Up", enum: "TOP_UP" },
+    { title: "Withdrawal", enum: "WITHDRAWAL" },
+]
