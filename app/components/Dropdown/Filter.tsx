1ee03c09f0ae44747b8299df38595970f856b614
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-05-14 22:20:29 +0100

    Optimized filter dropdown component

--- app/components/Dropdown/Filter.tsx
+++ app/components/Dropdown/Filter.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useClickAway, useToggle } from "ahooks";
 import { useRef, useState } from "react";
+import { FiArrowUpRight } from "react-icons/fi";
 import { IoIosCheckbox, IoMdArrowDropdown } from "react-icons/io";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 import { twMerge } from "tailwind-merge";
@@ -44,19 +45,21 @@ const FilterDropdown = ({ title, options, extendedButtonClassName, extendedConta
             {open && (
                 <div 
                     ref={dropdownRef}
-                    className="fixed top-[var(--dropdown-top)] left-[var(--dropdown-left)] px-2.5 py-[15px] max-h-[350px] 
+                    className="fixed top-[var(--dropdown-top)] left-[var(--dropdown-left)] px-2.5 max-h-[350px] 
                     bg-dark-400 dropdown-box shadow-[-20px_4px_40px_0px_#000000] z-[110] overflow-y-auto"
                     style={{
                         '--dropdown-top': `${buttonRef.current?.getBoundingClientRect().top}px`,
                         '--dropdown-left': `${buttonRef.current?.getBoundingClientRect().right as number + 7}px`
                     } as React.CSSProperties}
                 >
-                    <button 
-                        className="text-body-tiny text-light-200 font-bold sticky top-0 pb-3 hover:text-primary-100"
-                        onClick={() => {}}
-                    >
-                        Clear Selection
-                    </button>
+                    <div className="w-full pb-3 pt-[15px] bg-dark-400 sticky top-0">
+                        <button 
+                            className="w-fit text-body-tiny text-light-200 font-bold hover:text-primary-100"
+                            onClick={() => {}}
+                        >
+                            Clear Selection
+                        </button>
+                    </div>
                     <ul className="flex flex-col gap-3 list-none items-start">
                         {options.map((option, index) => (
                             <li 
@@ -72,11 +75,48 @@ const FilterDropdown = ({ title, options, extendedButtonClassName, extendedConta
                                 <span className="text-body-small text-light-100">{option}</span>
                             </li>
                         ))}
+                        {options.map((option, index) => (
+                            <li 
+                                key={index} 
+                                className="flex items-center gap-2.5 cursor-pointer"
+                                onClick={() => toggleItems(`${index}`)}
+                            >
+                                {selectedItems[`${index}`] ? (
+                                    <IoIosCheckbox className="text-[18px] text-primary-100" />
+                                ) : (
+                                    <MdOutlineCheckBoxOutlineBlank className="text-[18px] text-dark-100" />
+                                )}
+                                <span className="text-body-small text-light-100">{option}</span>
+                            </li>
+                        ))}
+                        {options.map((option, index) => (
+                            <li 
+                                key={index} 
+                                className="flex items-center gap-2.5 cursor-pointer"
+                                onClick={() => toggleItems(`${index}`)}
+                            >
+                                {selectedItems[`${index}`] ? (
+                                    <IoIosCheckbox className="text-[18px] text-primary-100" />
+                                ) : (
+                                    <MdOutlineCheckBoxOutlineBlank className="text-[18px] text-dark-100" />
+                                )}
+                                <span className="text-body-small text-light-100">{option}</span>
+                            </li>
+                        ))}
                     </ul>
+                    <div className="w-full pt-3 pb-[15px] bg-dark-400 sticky bottom-0">
+                        <button 
+                            className="group w-fit flex items-center gap-[5px] text-primary-100 text-button-large font-extrabold"
+                            onClick={() => {}}
+                        >
+                            <span className="group-hover:text-light-100">Apply</span>
+                            <FiArrowUpRight className="text-2xl" />
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
     );
 }
- 
+
 export default FilterDropdown;
