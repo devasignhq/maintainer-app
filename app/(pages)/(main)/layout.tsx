13d4d71a0a90f5f74e23c25637ef29951eeab5fe
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-05-15 21:42:20 +0100

    Added select project menu UI

--- app/(pages)/(main)/layout.tsx
+++ app/(pages)/(main)/layout.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { HiOutlineSelector } from "react-icons/hi";
+import { HiOutlineSelector, HiPlus } from "react-icons/hi";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ROUTES } from "@/app/helper";
+import ButtonPrimary from "@/app/components/ButtonPrimary";
+import { useState } from "react";
+import { FaCheck } from "react-icons/fa6";
 
 export default function MainLayout({
     children,
@@ -12,20 +15,62 @@ export default function MainLayout({
 }>) {
     const currentPath = usePathname();
     const checkPath = (path: string) => currentPath.includes(path) || currentPath === path;
+    const [activeProject, setActiveProject] = useState(demoProject[0]);
 
     return (
         <main className="h-full w-full px-[6.75%] flex flex-col">
             <section className="pt-5 flex items-center justify-between">
                 <img src="/davasign-logo.svg" alt="DevAsign" className="h-auto w-auto" />
-                <div className="flex items-center gap-2.5 text-light-100">
-                    <div className="flex items-center gap-[15px] text-headline-small">
-                        {/* <img src="/project-logo.png" alt="PL" className="h-auto w-auto" /> */}
-                        <span>PostgreSQL</span>
+                <div className="relative">
+                    <div className="flex items-center gap-2.5 text-light-100">
+                        <div className="flex items-center gap-[15px] text-headline-small">
+                            <span>PostgreSQL</span>
+                        </div>
+                        <div className="px-2.5 py-[7px] bg-dark-300 text-table-header">Free</div>
+                        <button className="cursor-pointer">
+                            <HiOutlineSelector className="text-2xl text-[#BCBCBC]" />
+                        </button>
                     </div>
-                    <div className="px-2.5 py-[px] bg-dark-300 text-table-header">Free</div>
-                    <button className="cursor-pointer">
-                        <HiOutlineSelector className="text-2xl text-[#BCBCBC]" />
-                    </button>
+                    <div className="absolute -left-12 top-[200%] w-[250px] p-[15px] bg-dark-400 border border-dark-200 shadow-[-20px_4px_40px_0px_#000000]">
+                        <div className="w-full flex flex-col gap-2.5">
+                            {demoProject.map((item) => {
+                                return activeProject.name === item.name ? (
+                                    <button
+                                        key={item.name}
+                                        className="pb-2.5 flex items-center justify-between gap-2.5 border-b border-dark-200"
+                                    >
+                                        <div className="flex items-center gap-2.5">
+                                            <p className="text-body-medium text-light-100">{item.name}</p>
+                                            <div className={`px-2.5 py-[7px] text-body-small ${item.plan === "Free" ? "text-light-100 bg-dark-300" : "text-dark-500 bg-primary-100"}`}>
+                                                {item.plan}
+                                            </div>
+                                        </div>
+                                        <FaCheck className="text-xl text-light-100" />
+                                    </button>
+                                ):(
+                                    <button
+                                        key={item.name}
+                                        className="group flex items-center justify-between gap-2.5"
+                                        onClick={() => setActiveProject(item)}
+                                    >
+                                        <p className="text-body-medium text-dark-100 group-hover:text-light-100">{item.name}</p>
+                                        <div className={`px-2.5 py-[7px] text-body-small ${item.plan === "Free" ? "text-light-100 bg-dark-300" : "text-dark-500 bg-primary-100"}`}>
+                                            {item.plan}
+                                        </div>
+                                    </button>
+                                )
+                            })}
+                        </div>
+                        <ButtonPrimary
+                            format="OUTLINE"
+                            text="New Project"
+                            sideItem={<HiPlus />}
+                            attributes={{
+                                onClick: () => {},
+                            }}
+                            extendedClassName="w-full mt-[30px]"
+                        />
+                    </div>
                 </div>
             </section>
             {!currentPath.includes(ROUTES.ONBOARDING) && (
@@ -57,4 +102,10 @@ const navItems = [
     { name: "Wallet", path: ROUTES.WALLET },
     // { name: "Contributors", path: ROUTES.CONTRIBUTORS },
     { name: "Settings", alias: "/settings", path: ROUTES.SETTINGS.GENERAL },
+];
+
+const demoProject = [
+    { name: "PostgreSQL", plan: "Free" },
+    { name: "DevAsign", plan: "Pro" },
+    { name: "Antiwork", plan: "Free" },
 ];
