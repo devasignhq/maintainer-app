dfeaa4fc8d7f98db951db8734e0fb2923a33a113
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-05-15 20:38:29 +0100

    Updated 'withdrawal' table view in wallet page

--- app/(pages)/(main)/wallet/tables/WithdrawalTable.tsx
+++ app/(pages)/(main)/wallet/tables/WithdrawalTable.tsx
@@ -1,49 +1,40 @@
 "use client";
-import Link from "next/link";
 
 const WithdrawalTable = () => {
     return (
         <>
             <thead>
                 <tr className="pb-[7px] border-b border-[#585858] text-table-header text-dark-100 flex gap-5">
-                    <th className="w-[50%] text-start">Payment Hash</th>
-                    <th className="w-[10%] text-start">Asset</th>
-                    <th className="w-[15%] text-start">Amount</th>
-                    <th className="w-[20%] text-start">Time</th>
+                    <th className="w-[45%]">Destination Address</th>
+                    <th className="w-[10.5%]">Asset</th>
+                    <th className="w-[17%]">Amount</th>
+                    <th className="w-[22.5%]">Time</th>
                 </tr>
             </thead>
             <tbody className="grow overflow-y-auto">
                 <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[50%] text-start text-light-200">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[10%] text-start">USDC</td>
-                    <td className="w-[15%] text-start text-primary-100">$245.00</td>
-                    <td className="w-[20%] text-start">03/02/2025 12:38 PM</td>
+                    <td className="w-[45%] text-light-200">GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</td>
+                    <td className="w-[10.5%]">XLM</td>
+                    <td className="w-[17%] text-primary-100">245.00</td>
+                    <td className="w-[22.5%]">03/02/2025 12:39 AM</td>
                 </tr>
                 <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[50%] text-start text-light-200">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[10%] text-start">USDC</td>
-                    <td className="w-[15%] text-start text-primary-100">$245.00</td>
-                    <td className="w-[20%] text-start">03/02/2025 12:38 PM</td>
+                    <td className="w-[45%] text-light-200">GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</td>
+                    <td className="w-[10.5%]">XLM</td>
+                    <td className="w-[17%] text-primary-100">34.30</td>
+                    <td className="w-[22.5%]">03/02/2025 12:39 AM</td>
                 </tr>
                 <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[50%] text-start text-light-200">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[10%] text-start">USDC</td>
-                    <td className="w-[15%] text-start text-primary-100">$245.00</td>
-                    <td className="w-[20%] text-start">03/02/2025 12:38 PM</td>
+                    <td className="w-[45%] text-light-200">GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</td>
+                    <td className="w-[10.5%]">XLM</td>
+                    <td className="w-[17%] text-primary-100">123.00</td>
+                    <td className="w-[22.5%]">03/02/2025 12:39 AM</td>
                 </tr>
                 <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[50%] text-start text-light-200">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[10%] text-start">USDC</td>
-                    <td className="w-[15%] text-start text-primary-100">$245.00</td>
-                    <td className="w-[20%] text-start">03/02/2025 12:38 PM</td>
+                    <td className="w-[45%] text-light-200">GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</td>
+                    <td className="w-[10.5%]">XLM</td>
+                    <td className="w-[17%] text-primary-100">690.00</td>
+                    <td className="w-[22.5%]">03/02/2025 12:39 AM</td>
                 </tr>
             </tbody>
         </>
