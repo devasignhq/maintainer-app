1aba440ed5111a34b8b09b6e0e51f6edfff1b917
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-05-15 20:37:59 +0100

    Updated 'all' table view in wallet page

--- app/(pages)/(main)/wallet/tables/AllTable.tsx
+++ app/(pages)/(main)/wallet/tables/AllTable.tsx
@@ -1,51 +1,45 @@
 "use client";
-import Link from "next/link";
 
 const AllTable = () => {
     return (
         <>
             <thead>
-                <tr className="pb-[7px] border-b border-[#585858] text-table-header text-dark-100 flex gap-5">
-                    <th className="w-[60%] pr-[80px] text-start">Payment Hash</th>
-                    <th className="w-[15%] text-start">Amount</th>
-                    <th className="w-[15%] text-start">Category</th>
+                <tr className="pb-[7px] border-b border-[#585858] text-table-header text-dark-100 grid grid-cols-10 gap-5">
+                    <th className="col-span-4">Category</th>
+                    <th className="col-span-3">Amount</th>
+                    <th className="col-span-3">Time</th>
                 </tr>
             </thead>
             <tbody className="grow overflow-y-auto">
-                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[60%] pr-[80px] text-start text-light-200 underline">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[15%] text-start">$245.00</td>
-                    <td className="w-[15%] text-start">Bounty</td>
+                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 grid grid-cols-10 gap-5">
+                    <td className="col-span-4">Bounty</td>
+                    <td className="col-span-3 text-indicator-500">-245.00 USDC</td>
+                    <td className="col-span-3">03/02/2025 12:39 AM</td>
                 </tr>
-                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[60%] pr-[80px] text-start text-light-200 underline">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[15%] text-start">$115.00</td>
-                    <td className="w-[15%] text-start">Top Up</td>
+                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 grid grid-cols-10 gap-5">
+                    <td className="col-span-4">Swap (USDC to XLM)</td>
+                    <td className="col-span-3">24.00 USDC to 100.00 XLM</td>
+                    <td className="col-span-3">03/02/2025 12:39 AM</td>
                 </tr>
-                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[60%] pr-[80px] text-start text-light-200 underline">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[15%] text-start">$45.00</td>
-                    <td className="w-[15%] text-start">Withdrawal</td>
+                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 grid grid-cols-10 gap-5">
+                    <td className="col-span-4">Withdrawal</td>
+                    <td className="col-span-3 text-indicator-500">-245.00 XLM</td>
+                    <td className="col-span-3">03/02/2025 12:39 AM</td>
                 </tr>
-                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[60%] pr-[80px] text-start text-light-200 underline">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[15%] text-start">$34.00</td>
-                    <td className="w-[15%] text-start">Bounty</td>
+                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 grid grid-cols-10 gap-5">
+                    <td className="col-span-4">Top Up</td>
+                    <td className="col-span-3 text-indicator-100">+245.00 XLM</td>
+                    <td className="col-span-3">03/02/2025 12:39 AM</td>
                 </tr>
-                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 flex items-center gap-5">
-                    <td className="w-[60%] pr-[80px] text-start text-light-200 underline">
-                        <Link href={""}>https://stellar.expert/explorer/public/account/GC7L4BVK43SBE3U445M2GXP2FY2AGYMEUO5V5B6GWWFHP2JOXBWOHOA3</Link>
-                    </td>
-                    <td className="w-[15%] text-start">$699.00</td>
-                    <td className="w-[15%] text-start">Tx Swap</td>
+                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 grid grid-cols-10 gap-5">
+                    <td className="col-span-4">Bounty</td>
+                    <td className="col-span-3 text-indicator-500">-245.00 USDC</td>
+                    <td className="col-span-3">03/02/2025 12:39 AM</td>
+                </tr>
+                <tr className="py-3.5 border-b border-dark-300 text-table-content text-light-100 grid grid-cols-10 gap-5">
+                    <td className="col-span-4">Swap (XLM to USDC)</td>
+                    <td className="col-span-3">367.00 XLM to 145.00 USDC</td>
+                    <td className="col-span-3">03/02/2025 12:39 AM</td>
                 </tr>
             </tbody>
         </>
