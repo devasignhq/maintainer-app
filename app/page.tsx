3688e668768c137024dec08c4666cfbe5f4f8a44
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-06-01 22:38:16 +0100

    Added user based routing in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { getCurrentUser } from "@/lib/firebase";
+import { ROUTES } from "@/app/utils/data";
+
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const user = await getCurrentUser();
+            if (user) {
+                router.push(ROUTES.TASKS);
+            } else {
+                router.push(ROUTES.ACCOUNT);
+            }
+        };
+
+        checkUser();
+    }, [router]);
+
     return (
-        <div className="grid min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <div className="min-h-screen grid place-content-center">
+            <Image
+                src="/davasign-logo.svg"
+                alt="DevAsign Logo"
+                width={0}
+                height={0}
+                priority
+                className="h-[60px] w-auto animate-pulse mb-10"
+            />
         </div>
     );
 }
