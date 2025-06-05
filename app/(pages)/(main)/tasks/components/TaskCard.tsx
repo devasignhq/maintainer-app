17e473ae9cd8143a8e17a39787ec4670fe96cc7d
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-06-05 22:09:19 +0100

    Refactored TaskCard component to use TaskDto for props, improving data handling and UI consistency

--- app/(pages)/(main)/tasks/components/TaskCard.tsx
+++ app/(pages)/(main)/tasks/components/TaskCard.tsx
@@ -1,15 +1,14 @@
 "use client";
+import { TaskDto } from "@/app/models/task.model";
+import { moneyFormat } from "@/app/utils/helper";
 
 type TaskCardProps = {
-    issueNumber: number;
-    label: string;
-    bounty: string;
-    title: string;
+    task: TaskDto;
     active?: boolean;
     onClick?: () => void;
 };
 
-const TaskCard = ({ issueNumber, label, bounty, title, active, onClick }: TaskCardProps) => {
+const TaskCard = ({ task, active, onClick }: TaskCardProps) => {
     return (
         <div 
             onClick={onClick}
@@ -20,14 +19,32 @@ const TaskCard = ({ issueNumber, label, bounty, title, active, onClick }: TaskCa
                     : "border-primary-200 hover:border-dark-200 hover:bg-dark-400"}
             `}
         >
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-1.5">
-                    <p className="text-body-tiny text-primary-400">#{issueNumber}</p>
-                    <div className="py-0.5 px-[7px] bg-primary-300 text-body-tiny font-bold text-light-200">{label}</div>
-                </div>
-                <p className="text-body-medium text-primary-400 font-bold">{bounty} USDC</p>
+            <div className="flex items-center gap-1.5">
+                <p className="text-body-tiny text-primary-400">#{task.issue.number}</p>
+                {task.issue.labels?.length > 0 && (
+                    <p className="py-0.5 px-[7px] bg-primary-300 text-body-tiny font-bold text-light-200 truncate">
+                        {task.issue.labels
+                            .map(label => label.name)
+                            .map((name, index, array) => 
+                                index === array.length - 1 ? name : `${name}, `
+                            )
+                            .join('')}
+                    </p>
+                )}
+                <p className="text-body-medium text-primary-400 font-bold ml-auto">{moneyFormat(task.bounty)} USDC</p>
             </div>
-            <p className="h-10 text-body-medium text-light-100 line-clamp-2">{title}</p>
+            <p 
+                className="text-body-medium text-light-100 overflow-hidden leading-5"
+                style={{
+                    display: '-webkit-box',
+                    WebkitLineClamp: 2,
+                    WebkitBoxOrient: 'vertical',
+                    maxHeight: '2.5rem', 
+                    lineHeight: '1.25rem'
+                }}
+            >
+                {task.issue.title}
+            </p>
         </div>
     );
 }
