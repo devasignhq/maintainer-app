6db9d87f77f6e0305f3362c1445af8f74bf6df75
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-06-07 04:54:35 +0100

    Completed functionality to approve a task submission request

---
+++ app/(pages)/(main)/tasks/modals/ApproveSubmissionModal.tsx
@@ -0,0 +1,115 @@
+"use client";
+import ButtonPrimary from "@/app/components/ButtonPrimary";
+import PopupModalLayout from "@/app/components/PopupModalLayout";
+import { TaskActivity } from "@/app/models/task.model";
+import { TaskAPI } from "@/app/services/task.service";
+import { formatDateTime } from "@/app/utils/helper";
+import { useRequest, useLockFn } from "ahooks";
+import Link from "next/link";
+import { useContext } from "react";
+import { FiArrowUpRight } from "react-icons/fi";
+import { toast } from 'react-toastify';
+import { ActiveTaskContext } from "../page";
+
+type ApproveSubmissionModalProps = {
+    taskActivity: TaskActivity;
+    toggleModal: () => void;
+    onSuccess: () => void;
+};
+
+const ApproveSubmissionModal = ({ taskActivity, toggleModal, onSuccess }: ApproveSubmissionModalProps) => {
+    const { activeTask, setActiveTask } = useContext(ActiveTaskContext);
+    
+    const { loading: approving, run: approveSubmission } = useRequest(
+        useLockFn(() => TaskAPI.validateCompletion(activeTask!.id)), 
+        {
+            manual: true,
+            onSuccess: (data) => {
+                toast.success("Submission approved successfully! Bounty disbursed to contributor.");
+                if (data) {
+                    setActiveTask({ ...activeTask!, ...data})
+                }
+                toggleModal();
+                onSuccess();
+            },
+            onError: (error: any) => {
+                if (error?.error?.message) {
+                    toast.error(error.error.message);
+                    return
+                }
+                toast.error("Failed to approve task submission. Please try again.");
+            }
+        }
+    );
+    
+    return (
+        <PopupModalLayout title="Approve Submission" toggleModal={toggleModal}>
+            <div className="space-y-[5px] text-body-tiny mt-5">
+                <div className="w-full flex items-center justify-between gap-10">
+                    <p className="text-primary-400">Pull Request:</p>
+                    <div className="flex items-center gap-1">
+                        <span className="text-light-100 underline truncate">
+                            {taskActivity.taskSubmission?.pullRequest}
+                        </span>
+                        <Link href={taskActivity.taskSubmission?.pullRequest || ""} target="_blank">
+                            <FiArrowUpRight className="text-2xl text-primary-100 hover:text-light-100" />
+                        </Link>
+                    </div>
+                </div>
+                {taskActivity.taskSubmission?.attachmentUrl && (
+                    <div className="w-full flex items-center justify-between gap-10">
+                        <p className="text-primary-400">Attachment:</p>
+                        <div className="flex items-center gap-1">
+                            <span className="text-light-100 underline truncate">
+                                {taskActivity.taskSubmission?.attachmentUrl}
+                            </span>
+                            <Link href={taskActivity.taskSubmission?.attachmentUrl || ""} target="_blank">
+                                <FiArrowUpRight className="text-2xl text-primary-100 hover:text-light-100" />
+                            </Link>
+                        </div>
+                    </div>
+                )}
+                <div className="w-full flex items-center justify-between gap-10">
+                    <p className="text-primary-400">Contributor:</p>
+                    <div className="flex items-center gap-1">
+                        <span className="text-light-100 underline truncate">
+                            @{taskActivity.taskSubmission?.user?.username}
+                        </span>
+                        <Link href={`https://github.com/${taskActivity.taskSubmission?.user?.username}`} target="_blank">
+                            <FiArrowUpRight className="text-2xl text-primary-100 hover:text-light-100" />
+                        </Link>
+                    </div>
+                </div>
+                <div className="w-full flex items-center justify-between gap-10">
+                    <p className="text-primary-400">Time:</p>
+                    <p className="text-light-100">{formatDateTime(taskActivity.createdAt)}</p>
+                </div>
+            </div>
+            <p className="my-5 text-body-medium text-dark-100">
+                Once you click “<span className="font-bold">Pay Contributor</span>” or merge the code 
+                successfully in GitHub, the bounty will be paid out to the contributor automatically.
+            </p>
+            <div className="flex gap-2.5">
+                <ButtonPrimary
+                    format="OUTLINE"
+                    text="Go Back"
+                    attributes={{
+                        onClick: toggleModal,
+                        disabled: approving
+                    }}
+                />
+                <ButtonPrimary
+                    format="SOLID"
+                    text={approving ? "Approving..." : "Pay Contributor"}
+                    sideItem={<FiArrowUpRight />}
+                    attributes={{
+                        onClick: approveSubmission,
+                        disabled: approving
+                    }}
+                />
+            </div>
+        </PopupModalLayout>
+    );
+}
+ 
+export default ApproveSubmissionModal;
---
+++ app/(pages)/(main)/tasks/modals/ReviewSubmissionModal.tsx
@@ -0,0 +1,118 @@
+"use client";
+import ButtonPrimary from "@/app/components/ButtonPrimary";
+import PopupModalLayout from "@/app/components/PopupModalLayout";
+import { TaskActivity } from "@/app/models/task.model";
+import Link from "next/link";
+import { FiArrowUpRight } from "react-icons/fi";
+import ApproveSubmissionModal from "./ApproveSubmissionModal";
+import { useToggle } from "ahooks";
+import { useContext } from "react";
+import { ActiveTaskContext } from "../page";
+import { moneyFormat, formatDateTime } from "@/app/utils/helper";
+
+type ReviewSubmissionModalProps = {
+    taskActivity: TaskActivity;
+    toggleModal: () => void;
+};
+
+const ReviewSubmissionModal = ({ taskActivity, toggleModal }: ReviewSubmissionModalProps) => {
+    const { activeTask } = useContext(ActiveTaskContext);
+    const [openApproveSubmissionModal, { toggle: toggleApproveSubmissionModal }] = useToggle(false);
+    
+    return (
+        <PopupModalLayout title="Review Contributor Submission" toggleModal={toggleModal}>
+            <p className="mt-2.5 text-body-medium text-dark-100">
+                Kindly review this pull request if it solves the problem as described in your GitHub issue. 
+                Once you merge the code successfully in GitHub, the bounty will be paid out to the contributor automatically.
+            </p>
+            <div className="w-full p-[15px] border border-dark-200 flex items-start gap-2.5 my-5">
+                <p className="text-body-tiny tracking-[-3%] text-primary-100" style={{ lineHeight: "20px" }}>
+                    #{activeTask?.issue.number}
+                </p>
+                <p 
+                    className="grow text-body-medium font-bold text-light-100 line-clamp-2"
+                    style={{
+                        display: '-webkit-box',
+                        WebkitLineClamp: 2,
+                        WebkitBoxOrient: 'vertical',
+                        maxHeight: '2.5rem', 
+                        lineHeight: '1.25rem'
+                    }}
+                >
+                    {activeTask?.issue.title}
+                </p>
+                <p className="text-body-tiny font-bold tracking-[-3%] text-primary-100" style={{ lineHeight: "20px" }}>
+                    {moneyFormat(activeTask?.bounty || "")} USDC
+                </p>
+            </div>
+            <div className="space-y-[5px] text-body-tiny">
+                <div className="w-full flex items-center justify-between gap-10">
+                    <p className="text-primary-400">Pull Request:</p>
+                    <div className="flex items-center gap-1">
+                        <span className="text-light-100 underline truncate">
+                            {taskActivity.taskSubmission?.pullRequest}
+                        </span>
+                        <Link href={taskActivity.taskSubmission?.pullRequest || ""} target="_blank">
+                            <FiArrowUpRight className="text-2xl text-primary-100 hover:text-light-100" />
+                        </Link>
+                    </div>
+                </div>
+                {taskActivity.taskSubmission?.attachmentUrl && (
+                    <div className="w-full flex items-center justify-between gap-10">
+                        <p className="text-primary-400">Attachment:</p>
+                        <div className="flex items-center gap-1">
+                            <span className="text-light-100 underline truncate">
+                                {taskActivity.taskSubmission?.attachmentUrl}
+                            </span>
+                            <Link href={taskActivity.taskSubmission?.attachmentUrl || ""} target="_blank">
+                                <FiArrowUpRight className="text-2xl text-primary-100 hover:text-light-100" />
+                            </Link>
+                        </div>
+                    </div>
+                )}
+                <div className="w-full flex items-center justify-between gap-10">
+                    <p className="text-primary-400">Contributor:</p>
+                    <div className="flex items-center gap-1">
+                        <span className="text-light-100 underline truncate">
+                            @{taskActivity.taskSubmission?.user?.username}
+                        </span>
+                        <Link href={`https://github.com/${taskActivity.taskSubmission?.user?.username}`} target="_blank">
+                            <FiArrowUpRight className="text-2xl text-primary-100 hover:text-light-100" />
+                        </Link>
+                    </div>
+                </div>
+                <div className="w-full flex items-center justify-between gap-10">
+                    <p className="text-primary-400">Time:</p>
+                    <p className="text-light-100">{formatDateTime(taskActivity.createdAt)}</p>
+                </div>
+            </div>
+            <div className="flex gap-2.5 mt-5">
+                <ButtonPrimary
+                    format="OUTLINE"
+                    text="Reject PR"
+                    attributes={{
+                        onClick: toggleModal,
+                    }}
+                />
+                <ButtonPrimary
+                    format="SOLID"
+                    text="Approve PR"
+                    sideItem={<FiArrowUpRight />}
+                    attributes={{
+                        onClick: toggleApproveSubmissionModal,
+                    }}
+                />
+            </div>
+            
+            {openApproveSubmissionModal && (
+                <ApproveSubmissionModal 
+                    taskActivity={taskActivity}
+                    toggleModal={toggleApproveSubmissionModal} 
+                    onSuccess={toggleModal} 
+                />
+            )}
+        </PopupModalLayout>
+    );
+}
+ 
+export default ReviewSubmissionModal;
