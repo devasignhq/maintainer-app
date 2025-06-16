6b20eebfca6f8dc33dffa09d60d47edd60ae7ec9
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-06-16 21:46:35 +0100

    Fixed issue with octokit configuration and added settings button to route users to their installation settings on GitHub

--- app/(pages)/(main)/layout.tsx
+++ app/(pages)/(main)/layout.tsx
@@ -15,6 +15,8 @@ import { useLogoutUser } from "@/lib/firebase";
 import { createContext, useState } from "react";
 import { githubApp } from "@/app/services/github.service";
 import { InstallationOctokit } from "@/app/models/github.model";
+import { FiSettings } from "react-icons/fi";
+import { openInNewTab } from "@/app/utils/helper";
 
 export const OctokitContext = createContext<InstallationOctokit | null>(null);
 
@@ -28,7 +30,7 @@ export default function MainLayout({
     const checkPath = (path: string) => currentPath.includes(path) || currentPath === path;
     const { menuButtonRef, menuRef, openMenu, toggleMenu } = usePopup();
     const { 
-        InstallationList, 
+        installationList, 
         activeInstallation,
         setInstallationList,
         setActiveInstallation
@@ -50,7 +52,7 @@ export default function MainLayout({
         }
     );
 
-    useAsyncEffect(useLockFn(async () => {
+    useAsyncEffect(async () => {
         if (!activeInstallation) {
             setOctokit(null);
             return
@@ -58,96 +60,104 @@ export default function MainLayout({
 
         const octokit = await githubApp.getInstallationOctokit(Number(activeInstallation.id));
         setOctokit(octokit);
-    }), [activeInstallation]);
+    }, [activeInstallation]);
 
     return (
         <main className="h-full w-full px-[6.75%] flex flex-col">
             <section className="pt-5 flex items-center justify-between">
                 <img src="/davasign-logo.svg" alt="DevAsign" className="h-auto w-auto" />
                 <div className="flex items-center gap-2.5">
-                    <div className="relative">
-                        <div className="flex items-center gap-2.5 text-light-100">
-                            <span className="flex items-center gap-[15px] text-headline-small">
-                                {activeInstallation?.account.login}
-                            </span>
-                            <div className="px-2.5 py-[7px] bg-dark-300 text-table-header">
-                                {activeInstallation?.subscriptionPackage?.name}
+                    {activeInstallation && <>
+                        <div className="relative">
+                            <div className="flex items-center gap-2.5 text-light-100">
+                                <span className="flex items-center gap-[15px] text-headline-small">
+                                    {activeInstallation.account.login}
+                                </span>
+                                <div className="px-2.5 py-[7px] bg-dark-300 text-table-header">
+                                    {activeInstallation.subscriptionPackage?.name}
+                                </div>
+                                <button 
+                                    ref={menuButtonRef}
+                                    onClick={toggleMenu}
+                                >
+                                    <HiOutlineSelector className="text-2xl text-[#BCBCBC]" />
+                                </button>
                             </div>
-                            <button 
-                                ref={menuButtonRef}
-                                onClick={toggleMenu}
-                            >
-                                <HiOutlineSelector className="text-2xl text-[#BCBCBC]" />
-                            </button>
-                        </div>
-                        {openMenu && (
-                            <div 
-                                ref={menuRef}
-                                className="absolute -left-6 top-[calc(100%+20px)] z-10 w-[250px] p-[15px] 
-                                    bg-dark-400 border border-dark-200 shadow-[-20px_4px_40px_0px_#000000]"
-                            >
-                                <div className="w-full flex flex-col gap-2.5">
-                                    {fetchingInstallations ? (
-                                        <>
-                                            {[...Array(3)].map((_, i) => (
-                                                <div
-                                                    key={i}
-                                                    className="h-8 w-full bg-dark-300 rounded animate-pulse"
-                                                />
-                                            ))}
-                                        </>
-                                    ) : (
-                                        InstallationList.map((installation) => {
-                                            return activeInstallation?.id === installation.id ? (
-                                                <button
-                                                    key={installation.id}
-                                                    className="pb-2.5 flex items-center justify-between gap-2.5 border-b border-dark-200"
-                                                >
-                                                    <div className="flex items-center gap-2.5">
-                                                        <p className="text-body-medium text-light-100">{installation.account.login}</p>
+                            {openMenu && (
+                                <div 
+                                    ref={menuRef}
+                                    className="absolute -left-6 top-[calc(100%+20px)] z-10 w-[250px] p-[15px] 
+                                        bg-dark-400 border border-dark-200 shadow-[-20px_4px_40px_0px_#000000]"
+                                >
+                                    <div className="w-full flex flex-col gap-2.5">
+                                        {fetchingInstallations ? (
+                                            <>
+                                                {[...Array(3)].map((_, i) => (
+                                                    <div
+                                                        key={i}
+                                                        className="h-8 w-full bg-dark-300 rounded animate-pulse"
+                                                    />
+                                                ))}
+                                            </>
+                                        ) : (
+                                            installationList.map((installation) => {
+                                                return activeInstallation.id === installation.id ? (
+                                                    <button
+                                                        key={installation.id}
+                                                        className="pb-2.5 flex items-center justify-between gap-2.5 border-b border-dark-200"
+                                                    >
+                                                        <div className="flex items-center gap-2.5">
+                                                            <p className="text-body-medium text-light-100">{installation.account.login}</p>
+                                                            <div className={`px-2.5 py-[7px] text-body-small 
+                                                                ${installation.subscriptionPackage?.price === 0 
+                                                                    ? "text-light-100 bg-dark-300" 
+                                                                    : "text-dark-500 bg-primary-100"}` 
+                                                            }>
+                                                                {installation.subscriptionPackage?.name}
+                                                            </div>
+                                                        </div>
+                                                        <FaCheck className="text-xl text-light-100" />
+                                                    </button>
+                                                ) : (
+                                                    <button
+                                                        key={installation.id}
+                                                        className="group flex items-center justify-between gap-2.5"
+                                                        onClick={() => setActiveInstallation(installation)}
+                                                    >
+                                                        <p className="text-body-medium text-dark-100 group-hover:text-light-100">
+                                                            {installation.account.login}
+                                                        </p>
                                                         <div className={`px-2.5 py-[7px] text-body-small 
                                                             ${installation.subscriptionPackage?.price === 0 
-                                                                ? "text-light-100 bg-dark-300" 
-                                                                : "text-dark-500 bg-primary-100"}` 
+                                                            ? "text-light-100 bg-dark-300" 
+                                                            : "text-dark-500 bg-primary-100"}` 
                                                         }>
                                                             {installation.subscriptionPackage?.name}
                                                         </div>
-                                                    </div>
-                                                    <FaCheck className="text-xl text-light-100" />
-                                                </button>
-                                            ) : (
-                                                <button
-                                                    key={installation.id}
-                                                    className="group flex items-center justify-between gap-2.5"
-                                                    onClick={() => setActiveInstallation(installation)}
-                                                >
-                                                    <p className="text-body-medium text-dark-100 group-hover:text-light-100">
-                                                        {installation.account.login}
-                                                    </p>
-                                                    <div className={`px-2.5 py-[7px] text-body-small 
-                                                        ${installation.subscriptionPackage?.price === 0 
-                                                        ? "text-light-100 bg-dark-300" 
-                                                        : "text-dark-500 bg-primary-100"}` 
-                                                    }>
-                                                        {installation.subscriptionPackage?.name}
-                                                    </div>
-                                                </button>
-                                            )
-                                        })
-                                    )}
+                                                    </button>
+                                                )
+                                            })
+                                        )}
+                                    </div>
+                                    <Link href={ROUTES.INSTALLATION.NEW} target="_blank">
+                                        <ButtonPrimary
+                                            format="OUTLINE"
+                                            text="New Installation"
+                                            sideItem={<HiPlus />}
+                                            attributes={{ type: "button" }}
+                                            extendedClassName="w-full mt-[30px]"
+                                        />
+                                    </Link>
                                 </div>
-                                <Link href={ROUTES.CONFIG.NEW_INSTALLATION} target="_blank">
-                                    <ButtonPrimary
-                                        format="OUTLINE"
-                                        text="New Installation"
-                                        sideItem={<HiPlus />}
-                                        attributes={{ type: "button" }}
-                                        extendedClassName="w-full mt-[30px]"
-                                    />
-                                </Link>
-                            </div>
-                        )}
-                    </div>
+                            )}
+                        </div>
+                        <button 
+                            title="Installation Settings" 
+                            onClick={() => openInNewTab(activeInstallation.htmlUrl)}
+                        >
+                            <FiSettings className="text-2xl text-light-100 hover:text-light-200" />
+                        </button>
+                    </>}
                     <button title="Logout" onClick={logoutUser}>
                         <TbLogout className="text-2xl text-light-100 hover:text-light-200" />
                     </button>
