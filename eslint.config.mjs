99fa8b85083d14830439d51d112ede2e6be1a7f5
lenny_malcolm0 <leonardmalcolm00@gmail.com>
2025-12-14 18:49:29 +0100

    chore: added more eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,16 +6,45 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "no-useless-escape": "off",
+            "no-case-declarations": "off",
+            "no-console": "warn",
+            "no-debugger": "error",
+            "no-duplicate-imports": "error",
+            "no-unused-expressions": "error",
+            "prefer-const": "error",
+            "no-var": "error",
+            "object-shorthand": "error",
+            "prefer-arrow-callback": "error",
+            "prefer-template": "error",
+            quotes: ["error", "double", { avoidEscape: true }],
+            semi: ["error", "always"],
+            "comma-dangle": ["error", "never"],
+            indent: ["error", 4],
+            "max-len": ["warn", { code: 300, ignoreUrls: true }],
+            "eol-last": ["error", "always"],
+
+            "@typescript-eslint/no-explicit-any": "error",
+            // Configure TypeScript unused vars rule to ignore variables starting with underscore
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_"
+                }
+            ],
+            "@typescript-eslint/no-namespace": "off",
+            "@typescript-eslint/no-empty-object-type": "off"
+        }
     }
-  }
 ];
 
 export default eslintConfig;
