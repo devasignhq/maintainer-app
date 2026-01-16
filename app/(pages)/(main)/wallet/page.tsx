164b41add9d1e6132a5de9626d8e2e541bf3f508
lenny_malcolm0 <leonardmalcolm00@gmail.com>
2026-01-16 21:28:09 +0100

    chore: fix errors caused by change in pagination format

--- app/(pages)/(main)/wallet/page.tsx
+++ app/(pages)/(main)/wallet/page.tsx
@@ -33,7 +33,6 @@ const Wallet = () => {
         activeInstallation?.id
     );
     const [activeTab, setActiveTab] = useState(tabs[0]);
-    const [currentPage, setCurrentPage] = useState(1);
     const [openWithdrawAssetModal, { toggle: toggleWithdrawAssetModal }] = useToggle(false);
     const [openFundWalletModal, { toggle: toggleFundWalletModal }] = useToggle(false);
     const [openSwapAssetModal, { toggle: toggleSwapAssetModal }] = useToggle(false);
@@ -61,7 +60,11 @@ const Wallet = () => {
         reload: reloadTransactions
     } = useInfiniteScroll<Data>(
         async (currentData) => {
-            const pageToLoad = currentData ? currentPage + 1 : 1;
+            if (!activeInstallation) {
+                return { list: [], pagination: null, pageToLoad: 1 };
+            }
+
+            const pageToLoad = currentData ? currentData.pageToLoad + 1 : 1;
 
             const category = activeTab.enum === "ALL"
                 ? ""
@@ -77,11 +80,10 @@ const Wallet = () => {
                 ...(category && { categories: category })
             });
 
-            setCurrentPage(pageToLoad);
-
             return {
-                list: response.transactions,
-                hasMore: response.hasMore
+                list: response.data,
+                pagination: response.pagination,
+                pageToLoad
             };
         },
         {
@@ -94,9 +96,9 @@ const Wallet = () => {
         if (!activeInstallation) return;
 
         try {
-            const { processed } = await WalletAPI.recordWalletTopups(activeInstallation.id);
+            const { data } = await WalletAPI.recordWalletTopups(activeInstallation.id);
 
-            if (processed > 0) {
+            if (data.processed > 0) {
                 reloadTransactions();
             }
         } catch { }
