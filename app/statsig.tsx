e6486a471fb09b843b6093ed97c175948574b598
lenny_malcolm0 <leonardmalcolm00@gmail.com>
2026-03-10 12:02:21 +0100

    refactor: optimize statsig user object

--- app/statsig.tsx
+++ app/statsig.tsx
@@ -1,14 +1,14 @@
 "use client";
-import React from "react";
+import { ReactNode, useMemo } from "react";
 import { LogLevel, StatsigProvider } from "@statsig/react-bindings";
 import useUserStore from "./state-management/useUserStore";
 
-export default function Statsig({ children }: { children: React.ReactNode }) {
+export default function Statsig({ children }: { children: ReactNode }) {
     const { currentUser } = useUserStore();
     const sdkKey = process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY;
 
     // The user object sent to Statsig
-    const user = {
+    const user = useMemo(() => ({
         // Only set userID if the user is authentically logged in
         ...(currentUser?.userId ? { userID: currentUser.userId } : {}),
         email: currentUser?.email || undefined,
@@ -17,7 +17,7 @@ export default function Statsig({ children }: { children: React.ReactNode }) {
             hasWallet: !!currentUser?.wallet,
             verified: currentUser?.verified
         }
-    };
+    }), [currentUser]);
 
     // If no sdkKey, return children without StatsigProvider
     if (!sdkKey) {
