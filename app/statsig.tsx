05299816c316ccecf13cb1ceecc94f1db73f7089
lenny_malcolm0 <leonardmalcolm00@gmail.com>
2026-03-10 11:30:38 +0100

    refactor: update statsig config; add email to user object

--- app/statsig.tsx
+++ app/statsig.tsx
@@ -5,24 +5,30 @@ import useUserStore from "./state-management/useUserStore";
 
 export default function Statsig({ children }: { children: React.ReactNode }) {
     const { currentUser } = useUserStore();
+    const sdkKey = process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY;
 
     // The user object sent to Statsig
     const user = {
         // Only set userID if the user is authentically logged in
         ...(currentUser?.userId ? { userID: currentUser.userId } : {}),
-        email: currentUser?.username, // use username as email context
         custom: {
             username: currentUser?.username,
+            email: currentUser?.email || undefined,
             hasWallet: !!currentUser?.wallet,
             verified: currentUser?.verified
         }
     };
 
+    // If no sdkKey, return children without StatsigProvider
+    if (!sdkKey) {
+        return <>{children}</>;
+    }
+
     return (
         <StatsigProvider
-            sdkKey={process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!}
+            sdkKey={sdkKey}
             user={user}
-            options={{ logLevel: LogLevel.Debug }}
+            options={{ logLevel: process.env.NODE_ENV === "development" ? LogLevel.Debug : LogLevel.Error }}
         >
             {children}
         </StatsigProvider>
