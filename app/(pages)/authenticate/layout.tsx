d57bfb7fe7f347820ac62c41511c8d843d189222
lenny_malcolm0 <leonardmalcolm00@gmail.com>
2026-03-17 15:21:08 +0100

    refactor: update auth page to desktop only

--- app/(pages)/authenticate/layout.tsx
+++ app/(pages)/authenticate/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import { useViewPort } from "@/app/utils/hooks";
+import { TbBrowser } from "react-icons/tb";
 
 export default function AuthLayout({
     children
@@ -10,65 +11,88 @@ export default function AuthLayout({
     const viewPortWidth = useViewPort();
 
     return (
-        <main className="h-full w-full flex">
-            <section className={`h-full sm:w-[42%] w-full text-light-100 max-sm:flex max-sm:flex-col max-sm:justify-between 
-                ${viewPortWidth <= 688 ? "auth-gradient" : ""}`}
-            >
-                <div className="sm:h-full sm:w-full sm:pl-[16%] max-sm:px-5 pt-5">
-                    <img src="/davasign-logo.svg" alt="DevAsign" className="h-auto w-auto" />
-                    {children}
-                </div>
-                <div className="sm:hidden pb-[80px]">
-                    <div className="flex items-center gap-2.5 justify-center">
-                        <img
-                            src="/davasign-logo.svg"
-                            alt="DevAsign"
-                            className="h-auto lg:w-auto w-[100px]"
-                        />
-                        <img
-                            src="/x.svg"
-                            alt="X"
-                            className="h-auto lg:w-auto w-[10px]"
-                        />
-                        <img
-                            src="/scf-logo.svg"
-                            alt="Stellar Community Fund"
-                            className="h-auto lg:w-auto sm:w-[130px] w-[115px]"
-                        />
+        <main className="h-full w-full">
+            <div className="hidden xl:flex h-full w-full">
+                <section className={`h-full w-[42%] text-light-100 max-sm:flex max-sm:flex-col 
+                    max-sm:justify-between ${viewPortWidth <= 688 ? "auth-gradient" : ""}`}
+                >
+                    <div className="sm:h-full sm:w-full sm:pl-[16%] max-sm:px-5 pt-5">
+                        <img src="/davasign-logo.svg" alt="DevAsign" className="h-auto w-auto" />
+                        {children}
                     </div>
-                    <p className="sm:text-body-medium text-body-tiny text-light-100 text-center mt-[30px]">
-                        Backed by Stellar Community Fund (SCF). <br />
-                        We’re the infrastructure powering fair compensation <br />
-                        for open-source contribution.
-                    </p>
-                </div>
-            </section>
-            <section className="max-sm:hidden h-full w-[58%] auth-gradient grid place-content-center">
-                <div>
-                    <div className="flex items-center gap-2.5 justify-center">
-                        <img 
-                            src="/davasign-logo.svg" 
-                            alt="DevAsign" 
-                            className="h-auto lg:w-auto w-[100px]"
-                        />
-                        <img
-                            src="/x.svg"
-                            alt="X"
-                            className="h-auto lg:w-auto w-[10px]"
-                        />
-                        <img
-                            src="/scf-logo.svg"
-                            alt="Stellar Community Fund"
-                            className="h-auto lg:w-auto w-[130px]"
-                        />
+                    <div className="sm:hidden pb-[80px]">
+                        <div className="flex items-center gap-2.5 justify-center">
+                            <img
+                                src="/davasign-logo.svg"
+                                alt="DevAsign"
+                                className="h-auto lg:w-auto w-[100px]"
+                            />
+                            <img
+                                src="/x.svg"
+                                alt="X"
+                                className="h-auto lg:w-auto w-[10px]"
+                            />
+                            <img
+                                src="/scf-logo.svg"
+                                alt="Stellar Community Fund"
+                                className="h-auto lg:w-auto sm:w-[130px] w-[115px]"
+                            />
+                        </div>
+                        <p className="sm:text-body-medium text-body-tiny text-light-100 text-center mt-[30px]">
+                            Backed by Stellar Community Fund (SCF). <br />
+                            We’re the infrastructure powering fair compensation <br />
+                            for open-source contribution.
+                        </p>
                     </div>
-                    <p className="text-body-medium text-light-100 text-center mt-[30px]">
-                        Backed by Stellar Community Fund (SCF). <br />
-                        We’re the infrastructure powering fair compensation <br className="md:hidden" />
-                        for open-source contribution.
-                    </p>
+                </section>
+                <section className="max-sm:hidden h-full w-[58%] auth-gradient grid place-content-center">
+                    <div>
+                        <div className="flex items-center gap-2.5 justify-center">
+                            <img 
+                                src="/davasign-logo.svg" 
+                                alt="DevAsign" 
+                                className="h-auto lg:w-auto w-[100px]"
+                            />
+                            <img
+                                src="/x.svg"
+                                alt="X"
+                                className="h-auto lg:w-auto w-[10px]"
+                            />
+                            <img
+                                src="/scf-logo.svg"
+                                alt="Stellar Community Fund"
+                                className="h-auto lg:w-auto w-[130px]"
+                            />
+                        </div>
+                        <p className="text-body-medium text-light-100 text-center mt-[30px]">
+                            Backed by Stellar Community Fund (SCF). <br />
+                            We’re the infrastructure powering fair compensation <br className="md:hidden" />
+                            for open-source contribution.
+                        </p>
+                    </div>
+                </section>
+            </div>
+
+            <div className="mobile-tablet-only h-full flex flex-col justify-between">
+                <img 
+                    src="/davasign-logo.svg" 
+                    alt="DevAsign" 
+                    className="h-[25px] w-auto absolute top-5 left-5" 
+                />
+                <div className="grow grid place-content-center">
+                    <div className="mb-14">
+                        <TbBrowser className="text-3xl text-primary-100 mx-auto" />
+                        <h2 className="text-body-medium text-light-200 my-5 text-center">
+                            Switch to Desktop
+                        </h2>
+                        <p className="text-body-tiny text-light-100 text-center">
+                            DevAsign is only accessible via desktop <br />
+                            browsers for now. Tablet and mobile view will <br />
+                            be available soon.
+                        </p>
+                    </div>
                 </div>
-            </section>
+            </div>
         </main>
     );
 }
